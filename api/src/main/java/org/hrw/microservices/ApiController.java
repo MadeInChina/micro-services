77990ac96bcf23661cdc11724630bf65e960d0a3
slamhan <slamhan1987@hotmail.com>
2019-06-15 14:15:25 +0800

    init

---
+++ api/src/main/java/org/hrw/microservices/ApiController.java
@@ -0,0 +1,14 @@
+package org.hrw.microservices;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+  @RequestMapping(path = "/ping", method = RequestMethod.POST)
+  public String ping() {
+    return "pong";
+  }
+}
