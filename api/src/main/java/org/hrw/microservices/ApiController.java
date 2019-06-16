0a9570d56a8d9fb70f5d44a17f657c6a6b7616e7
slamhan <slamhan1987@hotmail.com>
2019-06-16 15:07:02 +0800

    add tags

--- api/src/main/java/org/hrw/microservices/ApiController.java
+++ api/src/main/java/org/hrw/microservices/ApiController.java
@@ -1,10 +1,12 @@
 package org.hrw.microservices;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableDiscoveryClient
 public class ApiController {
 
   @RequestMapping(path = "/ping", method = RequestMethod.POST)
